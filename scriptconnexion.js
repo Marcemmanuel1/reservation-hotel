7d1db3ec0bf4a4b91a160b6a71d240771c622045
Marcemmanuel1 <108848425+Marcemmanuel1@users.noreply.github.com>
2025-04-01 22:08:42 +0000

    Update scriptconnexion.js

--- scriptconnexion.js
+++ scriptconnexion.js
@@ -1,5 +1,1 @@
- // Animation de chargement
- document.addEventListener('DOMContentLoaded', function() {
-    // Vous pouvez ajouter ici des fonctionnalités JavaScript supplémentaires
-    // comme la validation du formulaire si nécessaire
-});
+
